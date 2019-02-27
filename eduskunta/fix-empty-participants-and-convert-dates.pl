7c6cd2efff81f6b13add8cb6ec77ff1e9599abac
eaxelson <erik.axelson@helsinki.fi>
2019-02-27 17:02:37 +0200

    Remove extra quotes around replace. Actually print the line.

--- eduskunta/fix-empty-participants-and-convert-dates.pl
+++ eduskunta/fix-empty-participants-and-convert-dates.pl
@@ -7,8 +7,8 @@ use open qw(:std :utf8);
 foreach my $line ( <STDIN> ) {
 
     # remove empty participants in utterances
-    $line =~ 's/<utterance participant="" /<utterance /';
+    $line =~ s/<utterance participant="" /<utterance /;
     # convert dates to YYYY-MM-DD format
-    $line =~ 's/date="([0-9]{2})\.([0-9]{2})\.([0-9]{4})"/date="$3\-$2\-$1"/';
-
+    $line =~ s/date="([0-9]{2})\.([0-9]{2})\.([0-9]{4})"/date="$3\-$2\-$1"/;
+    print $line;
 }
