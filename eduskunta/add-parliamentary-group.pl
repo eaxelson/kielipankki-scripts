21b71995b3b87836fcae076590f5954290d59178
eaxelson <erik.axelson@helsinki.fi>
2019-02-27 15:55:31 +0200

    Exctract possible role in participant. Add script for fixing dates and empty participants.

--- eduskunta/add-parliamentary-group.pl
+++ eduskunta/add-parliamentary-group.pl
@@ -7,6 +7,7 @@ use strict;
 use warnings;
 use open qw(:std :utf8);
 
+# if parliamentary group is found, remove it from value of attribute 'participant'
 my $remove_group_from_participant = "true";
 
 foreach my $line ( <STDIN> ) {
@@ -22,11 +23,18 @@ foreach my $line ( <STDIN> ) {
 	    print "\n";
 	    exit 1;
 	}
-	# extract value of participant and modify it to get group
-	my $group = $line;
-	$group =~ s/.*participant="([^"]+)".*/$1/;
+
+	# get rid of extra space and use only ordinary space
+	$line =~ s/\h/ /g;
+	$line =~ s/ +/ /g;
+	$line =~ s/ "/"/g;
+
+	# extract value of participant and modify it to get additional attributes
+	my $participant = $line;
+	$participant =~ s/.*participant="([^"]+)".*/$1/;
 
 	# group given after slash, e.g. "/kesk", "/kok", "/sd" etc.
+	my $group = $participant;
 	if ( $group =~ /\// )
 	{
 	    $group =~ s/.*\/([a-öA-Ö0-9]*).*/$1/;
@@ -68,23 +76,54 @@ foreach my $line ( <STDIN> ) {
 
 	# remove content in parentheses in participant
 	$line =~ s/(participant="[^"\(]+)(\([^"\)]+\))/$1/g;
-
-	# and append it to attribute 'extra'
+	# and append it to attribute 'remark'
 	if ( defined $2 )
 	{
-	    my $extra = $2;
-	    $extra =~ s/\((.*)\)/$1/;
-	    $line =~ s/>/ extra="$extra">/;
+	    my $remark = $2;
+	    $remark =~ s/\((.*)\)/$1/;
+	    $line =~ s/>/ remark="$remark">/;
+	}
+
+	# exctract possible role in participant
+	# and append it to attribute 'role', but do not remove it
+	my $possible_role = $participant;
+	if ( $possible_role =~ /[A-Ö][a-ö]+(\- ja [a-ö]+)?ministeri/ ||
+	     $possible_role =~ /Eduskunnan oikeusasiamies/ ||
+	     $possible_role =~ /Ensimm\x{00E4}inen puhemies/ ||
+	     $possible_role =~ /Ensimm\x{00E4}inen varapuhemies/ ||
+	     $possible_role =~ /Ik\x{00E4}puhemies/ ||
+	     $possible_role =~ /Puhemies/ ||
+	     $possible_role =~ /Puhuja/ ||
+	     $possible_role =~ /Toinen varapuhemies/ ||
+	     $possible_role =~ /Valtioneuvoston apulaisoikeuskansleri/ ||
+	     $possible_role =~ /Valtioneuvoston oikeuskansleri/ ||
+	     $possible_role =~ /varaj\x{00E4}senenään( kansanedustaja)/ )
+	{
+	    print $possible_role;
+	    if ( defined $& )
+	    {
+		my $role = $&;
+		$line =~ s/>/ role="$role">/;
+	    }
+	}
+	elsif ( $possible_role =~ /Ensimm\x{00E4}inen/ ||
+		$possible_role =~ /Toinen/ )
+	{
+	    if ( defined $& )
+	    {
+		my $role = $&;
+		$line =~ s/>/ role="$role \(vara\)puhemies">/;
+	    }
+	}
+	elsif ( $possible_role =~ /Ensimma?inen varapuhemies/ )
+	{
+	    $line =~ s/>/ role="Ensimm\x{00E4}inen varapuhemies">/;
 	}
 
 	# Remove ordinal before participant name
 	$line =~ s/participant="[0-9]+\. /participant="/g;
-
-	# get rid of extra space and use only ordinary space
-	$line =~ s/\h/ /g;
-	$line =~ s/ +/ /g;
-	$line =~ s/ "/"/g;
     }
 
     print $line;
 }
+
---
+++ eduskunta/fix-empty-participants-and-convert-dates.pl
@@ -0,0 +1,14 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+use open qw(:std :utf8);
+
+foreach my $line ( <STDIN> ) {
+
+    # remove empty participants in utterances
+    $line =~ 's/<utterance participant="" /<utterance /';
+    # convert dates to YYYY-MM-DD format
+    $line =~ 's/date="([0-9]{2})\.([0-9]{2})\.([0-9]{4})"/date="$3\-$2\-$1"/';
+
+}
