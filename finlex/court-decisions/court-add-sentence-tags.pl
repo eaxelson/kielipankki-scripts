e301694b62f5f87aff8730ce7f79587df0fa0639
eaxelson <erik.axelson@helsinki.fi>
2019-03-14 15:35:45 +0200

    Add option --comma-threshold. Include sentence number in warning and error messages.

--- finlex/court-decisions/court-add-sentence-tags.pl
+++ finlex/court-decisions/court-add-sentence-tags.pl
@@ -9,11 +9,14 @@ my $first_line = "true";
 my $end_tag_printed = "false";
 my $words = 0; # number of words in a sentence, including punctuation
 my $limit = 100000;
+my $comma_threshold = 100000;
 
 foreach (@ARGV)
 {
     if ( $_ eq "--limit" ) { $limit = -1; }
+    elsif ( $_ eq "--comma-threshold" ) { $comma_threshold = -1; }
     elsif ( $limit == -1 ) { $limit = $_; }
+    elsif ( $comma_threshold == -1 ) { $comma_threshold = $_; }
     else { print "Error: argument "; print $_; print " not recognized\n"; exit 1; }
 }
 
@@ -24,14 +27,14 @@ foreach my $line ( <STDIN> ) {
     {
 	;
     }
-    elsif ( $line =~ /^\.$/)
+    elsif ( $line =~ /^(\.|\;|\:)$/ || ( $words > $comma_threshold && $line =~ /^,$/ ) )
     {
 	if ($first_line eq "true")
 	{
-	    print STDERR "court-add-sentence-tags.pl: error: first non-empty line in sentence is '.', exiting.\n";
+	    print STDERR "court-add-sentence-tags.pl: error: first non-empty line in sentence "; print STDERR $sentence_number; print STDERR " is '.', ';' or ':', exiting.\n";
 	    exit 1;
 	}
-	if ( ++$words > $limit ) { print STDERR "Warning: number of words in a sentence is "; print STDERR $words; print STDERR "\n"; }
+	if ( ++$words > $limit ) { print STDERR "Warning: number of words in sentence "; print STDERR $sentence_number; print STDERR " is "; print STDERR $words; print STDERR "\n"; }
 	print $line;
 	print '</sentence>';
 	print "\n";
