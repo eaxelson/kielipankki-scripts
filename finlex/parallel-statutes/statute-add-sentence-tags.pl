f83f69f24efcc2637ca71f1a109d8d00faf83328
eaxelson <erik.axelson@helsinki.fi>
2019-03-14 15:33:07 +0200

    Add missing parentheses to regex.

--- finlex/parallel-statutes/statute-add-sentence-tags.pl
+++ finlex/parallel-statutes/statute-add-sentence-tags.pl
@@ -67,7 +67,7 @@ foreach my $line ( <STDIN> ) {
 	if ( $delayed eq "true" ) { $sentence .= $line;	} else { print $line; }
     }
     # end of sentence
-    elsif ( $line =~ /^\.$/ || $line =~ /^\.\)$/ || $line =~ /^\.\]$/ || $line =~ /^\;$/ || $line =~ /^\:$/ || $line =~ /^\.\.\.$/  || ( $words > $tag_threshold && $line =~ /^<>$/ ) || ( $words > $comma_threshold && $line =~ /^,$/ ) || ($words > $threshold && $line =~ /^\-|\x{002D}|\x{2013}|\x{2014}|\x{2015}$/ ) )
+    elsif ( $line =~ /^\.$/ || $line =~ /^\.\)$/ || $line =~ /^\.\]$/ || $line =~ /^\;$/ || $line =~ /^\:$/ || $line =~ /^\.\.\.$/  || ( $words > $tag_threshold && $line =~ /^<>$/ ) || ( $words > $comma_threshold && $line =~ /^,$/ ) || ($words > $threshold && $line =~ /^(\-|\x{002D}|\x{2013}|\x{2014}|\x{2015})$/ ) )
     {
 	unless ( $line =~ /^<>$/ )
 	{
