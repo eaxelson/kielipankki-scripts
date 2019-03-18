123116ad4712d1cbbe6b67a30a5b3b794b2b3fd9
eaxelson <erik.axelson@helsinki.fi>
2019-03-18 17:46:37 +0200

    Improve stats script.

--- finlex/parallel-statutes/get-link-stats.sh
+++ finlex/parallel-statutes/get-link-stats.sh
@@ -9,24 +9,30 @@ files=0
 different_numbers=0
 different_names=0
 
-for file in asd_par/fi/${year}/*.vrt
+for file_fi in asd/fi/${year}/*.vrt
 do
-    files=$(($files+1))
-    grep '<link' $file > tmp1
-    grep '<link' `echo $file | perl -pe 's/\/fi\//\/sv\//;'` > tmp2
-    links_fi=`wc -l tmp1 | cut -f1 -d' '`
-    links_sv=`wc -l tmp2 | cut -f1 -d' '`
-    if !( diff tmp1 tmp2 > /dev/null ); then
-	if [ "$links_fi" != "$links_sv" ]; then
-	    different_numbers=$(($different_numbers+1))
-	    #echo "different numbers of links: "$file
-	else
-	    different_names=$(($different_names+1))
-	    #echo "different names for links: "$file
+    file_sv=`echo $file_fi | perl -pe 's/\/fi\//\/sv\//;'`
+    if (ls $file_sv > /dev/null 2> /dev/null); then
+	files=$(($files+1))
+	grep '<link' $file_fi > tmp1
+	grep '<link' $file_sv > tmp2
+	links_fi=`wc -l tmp1 | cut -f1 -d' '`
+	links_sv=`wc -l tmp2 | cut -f1 -d' '`
+	if !( diff tmp1 tmp2 > tmp3 ); then
+	    if [ "$links_fi" != "$links_sv" ]; then
+		different_numbers=$(($different_numbers+1))
+		echo "different numbers of links: "$file_fi" "$file_sv
+		cat tmp3
+	    else
+		different_names=$(($different_names+1))
+		echo "different names for links:  "$file_fi" "$file_sv
+		cat tmp3
+	    fi
 	fi
     fi
 done
 
-echo "Files with different names for links: "$different_names
-echo "Files with different numbers of links: "$different_numbers
-echo "Files in total: "$files
+echo "Files with different numbers of links:                                  "$different_numbers
+echo "Files with equal numbers of links, but part of links differently named: "$different_names
+echo "Files with the same links:                                              "$(($files-$different_numbers-$different_names))
+echo "Files in total:                                                         "$files
