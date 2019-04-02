cef170c2afeefce1fc4a80184b777c31496bba9f
eaxelson <erik.axelson@helsinki.fi>
2019-04-02 13:41:27 +0300

    Skip <saa:(Osa|Luku|Pykala)TunnusKooste/> tags.

--- finlex/parallel-statutes-new/statutes-4.pl
+++ finlex/parallel-statutes-new/statutes-4.pl
@@ -12,8 +12,8 @@ my $saadosotsikkokooste = 0;
 my $delayed = "";
 
 while (<>) {
-    if (/^<saa:(Osa|Luku|Pykala)TunnusKooste/) { $tunnuskooste = 1; $delayed .= join('','<<paragraph type="heading">>',"\n"); }
-    elsif (/^<saa:SaadosOtsikkoKooste/) { $saadosotsikkokooste = 1; }
+    if (/^<saa:(Osa|Luku|Pykala)TunnusKooste[ >]/) { $tunnuskooste = 1; $delayed .= join('','<<paragraph type="heading">>',"\n"); }
+    elsif (/^<saa:SaadosOtsikkoKooste[ >]/) { $saadosotsikkokooste = 1; }
     elsif (/^<\/saa:(Osa|Luku|Pykala)TunnusKooste/) { $tunnuskooste = 0; $waiting = 1; }
     elsif (/^<\/saa:SaadosOtsikkoKooste/) { $saadosotsikkokooste = 0; }
     elsif ($tunnuskooste eq 1 || $saadosotsikkokooste eq 1) { $title .= $_; $title =~ s/\n/ /g; }
