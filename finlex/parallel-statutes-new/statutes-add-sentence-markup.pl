f2e98d9b26eae017e7db47469c1c1585276b62e7
eaxelson <erik.axelson@helsinki.fi>
2019-04-04 11:44:16 +0300

    Rewrite parts of script.

--- finlex/parallel-statutes-new/statutes-add-sentence-markup.pl
+++ finlex/parallel-statutes-new/statutes-add-sentence-markup.pl
@@ -11,91 +11,98 @@ use warnings;
 use open qw(:std :utf8);
 
 my $sentence_number = 0; # number of sentence
-my $first_line = "true"; # control sentence tags
-my $end_tag_printed = "true"; # control sentence tags
 my $sentence = ""; # the current sentence
 my $words = 0; # number of words in a sentence, including punctuation
 my $limit = 100000; # limit of sentence length, print warning if exceeded
 
 # interpret the following symbols as sentence separators if the respective threshold is exceeded:
-my $comma_threshold = 100000; # comma
-my $threshold = 100000; # hyphen, hyphen-minus, n dash, m dash and horizontal bar
+# my $comma_threshold = 100000; # comma
+# my $threshold = 100000; # hyphen, hyphen-minus, n dash, m dash and horizontal bar
 
-my $delayed = "false"; # delayed printing of sentences, required automatically if --limit is set
 my $filename = ""; # for more informative warning messages
 
 foreach (@ARGV)
 {
-    if ( $_ eq "--delayed" ) { $delayed = "true"; }
-    elsif ( $_ eq "--limit" ) { $limit = -1; $delayed = "true"; }
-    elsif ( $_ eq "--comma-threshold" ) { $comma_threshold = -1; }
-    elsif ( $_ eq "--threshold" ) { $threshold = -1; }
+    if ( $_ eq "--limit" ) { $limit = -1; }
+    # elsif ( $_ eq "--comma-threshold" ) { $comma_threshold = -1; }
+    # elsif ( $_ eq "--threshold" ) { $threshold = -1; }
     elsif ( $_ eq "--filename" ) { $filename = "next..."; }
-    elsif ( $_ eq "--help" || $_ eq "-h" ) { print "Usage: $0 [--limit LIMIT] [--comma-threshold CT] [--threshold T] [--delayed] [--filename FILENAME]\n"; exit 0; }
+    elsif ( $_ eq "--help" || $_ eq "-h" ) { print "Usage: $0 [--limit LIMIT] [--filename FILENAME]\n"; exit 0; }
+    # TODO [--comma-threshold CT] [--threshold T]
     elsif ( $limit == -1 ) { $limit = $_; }
-    elsif ( $comma_threshold == -1 ) { $comma_threshold = $_; }
-    elsif ( $threshold == -1 ) { $threshold = $_; }
+    # elsif ( $comma_threshold == -1 ) { $comma_threshold = $_; }
+    # elsif ( $threshold == -1 ) { $threshold = $_; }
     elsif ( $filename eq "next..." ) { $filename = $_; }
     else { print join("","Error: argument ",$_," not recognized\n"); exit 1; }
 }
 
 foreach my $line ( <STDIN> ) {
 
-    # do not process xml markup or count it as a word
-    if ( $line =~ /^</ )
+    # skip xml (including sentence boundary marker <>)
+    if ( $sentence eq "" && $line =~ /^</ )
     {
-	$sentence .= $line;
+	unless ($line =~ /^<>$/) { print $line; }
     }
     # end of sentence
-    elsif ( $line =~ /^\.$/ || $line =~ /^\.\)$/ || $line =~ /^\.\]$/ || $line =~ /^\;$/ || $line =~ /^\:$/ || $line =~ /^\.\.\.$/ || ( $words > $comma_threshold && $line =~ /^,$/ ) || ($words > $threshold && $line =~ /^(\-|\x{002D}|\x{2013}|\x{2014}|\x{2015})$/ ) )
+    # TODO: ( $words > $comma_threshold && $line =~ /^,$/ ) || ($words > $threshold && $line =~ /^(\-|\x{002D}|\x{2013}|\x{2014}|\x{2015})$/ ) )
+    elsif ( $line =~ /^\.$/ || $line =~ /^\.\)$/ || $line =~ /^\.\]$/ || $line =~ /^\;$/ || $line =~ /^\:$/ || $line =~ /^\.\.\.$/ || $line =~ /^</)
     {
-	$words++;
-	if ( $line =~ /^\.\)$/ )
+	if ($sentence eq "")
+	{
+	    print STDERR join('',"Error: sentence starts with '",$line,"' in file ",$filename,"'\n");
+	    exit 1;
+	}
+	if ( $line =~ /^</) # xml (including sentence boundary marker <>) -> no words
+	{
+	    $sentence .= "</sentence>\n";
+	    unless ($line =~ /^<>$/) { $sentence .= $line; } # print xml (excluding <>) after </sentence>
+	}
+	elsif ( $line =~ /^\.\)$/ ) # .) -> two words
 	{
 	    $sentence .= ".\n)\n";
+	    $words++; $words++;
+	    $sentence .= "</sentence>\n";
 	}
-	elsif ( $line =~ /^\.\]$/ )
+	elsif ( $line =~ /^\.\]$/ ) # .] -> two words
 	{
 	    $sentence .= ".\n]\n";
+	    $words++; $words++;
+	    $sentence .= "</sentence>\n";
 	}
-	else
+	else # one word
 	{
 	    $sentence .= $line;
+	    $words++;
+	    $sentence .= "</sentence>\n";
 	}
-	
-	$sentence .= '</sentence>';
+
 	if ( $words > $limit ) { print STDERR join("","warning: sentence length is ",$words," words in sentence number ",$sentence_number," in file ",$filename,"\n"); }
-	$sentence .= "\n";
 	print $sentence;
 	$sentence = "";
-	$end_tag_printed = "true";
-	$first_line = "true";
 	$words = 0;
     }
     else
     {
 	$words++;
-	if ($first_line eq "true")
+	if ($sentence eq "")
 	{
-	    $sentence .= join("",'<sentence n="',++$sentence_number,'">',"\n");
-	    $end_tag_printed = "false";
+	    $sentence = join('','<sentence n="',++$sentence_number,'">',"\n");
 	}
-	$first_line = "false";
 	$sentence .= $line;
     }
 }
 
 
-if ($end_tag_printed eq "false")
+if ($sentence ne "")
 {
-    # print STDERR "court-add-sentence-tags.pl: warning: missing '.' at the end of last sentence, adding a sentence end tag anyway.\n";
-    $sentence .= '</sentence>';
-    if ( $words > $limit ) { print STDERR join("","warning: sentence length is ",$words," words in sentence number ",$sentence_number," in file ",$filename,"\n"); }
-    $sentence .= "\n";
-    print $sentence;
+    print STDERR join('',"Error: last sentence in file ",$filename," could not be ended:\n",$sentence);
+    #$sentence .= '</sentence>';
+    #if ( $words > $limit ) { print STDERR join("","warning: sentence length is ",$words," words in sentence number ",$sentence_number," in file ",$filename,"\n"); }
+    #$sentence .= "\n";
+    #print $sentence;
 }
 else
 {
     # sentence might contain the last xml markup that has not yet been printed
-    print $sentence;
+    #print $sentence;
 }
