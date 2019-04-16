cfdf1af99063bd876731a1225b7165cf3d4079d6
eaxelson <erik.axelson@helsinki.fi>
2019-04-16 11:47:25 +0300

    Escape special characters in titles. Remove xml tags. Do not print empty titles.

--- finlex/court-decisions-new/court-mark-heading-paragraphs.pl
+++ finlex/court-decisions-new/court-mark-heading-paragraphs.pl
@@ -14,8 +14,35 @@ my $delayed = "";
 my $title = "";
 
 while (<>) {
-    if (/^<h[23][ >]/) { unless (/\/>$/) { $delayed .= "<<paragraph type=\"heading\">>\n"; } }
-    elsif (/^<\/h[23]>/) { $delayed .= "<</paragraph>>\n"; $title =~ s/\n//g; print join('',"<<title=\"",$title,"\">>\n"); print $delayed; $delayed = ""; $title = ""; }
-    elsif ($delayed ne "") { $delayed .= $_; $title .= $_; }
-    else { print; }
+    if (/^<h[23][ >]/)
+    {
+	unless (/\/>$/)
+	{
+	    $delayed .= "<<paragraph type=\"heading\">>\n";
+	}
+    }
+    elsif (/^<\/h[23]>/)
+    {
+	$delayed .= "<</paragraph>>\n";
+	$title =~ s/\n//g;
+	$title =~ s/'/&apos;/g;
+	$title =~ s/"/&quot;/g;
+	$title =~ s/<[^>]*>//g; # e.g. <em>
+	unless ($title eq "")
+	{
+	    print join('',"<<title=\"",$title,"\">>\n");
+	}
+	print $delayed;
+	$delayed = "";
+	$title = "";
+    }
+    elsif ($delayed ne "")
+    {
+	$delayed .= $_;
+	$title .= $_;
+    }
+    else
+    {
+	print;
+    }
 }
