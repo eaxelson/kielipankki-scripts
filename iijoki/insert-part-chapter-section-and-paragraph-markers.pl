e6d2959808a7f7cd4da838ee9c0263f6dd178880
eaxelson <erik.axelson@helsinki.fi>
2019-10-03 16:05:51 +0300

    Fix errors in script.

--- iijoki/insert-part-chapter-section-and-paragraph-markers.pl
+++ iijoki/insert-part-chapter-section-and-paragraph-markers.pl
@@ -6,6 +6,9 @@ use open qw(:std :utf8);
 
 my $empty_lines = 0;
 my $latest_element = "";
+my $first_part_encountered = "false";
+my $first_chapter_of_part = "true";
+my $first_section_of_chapter = "true";
 
 foreach my $line ( <STDIN> ) {    
 
@@ -13,28 +16,54 @@ foreach my $line ( <STDIN> ) {
     
     if ($line =~ /^((ENSIMM\x{00C4}INEN|TOINEN|((KOLMAS|NELJ\x{00C4}S|VIIDES|KUUDES|SEITSEM\x{00C4}S|KAHDEKSAS|YHDEKS\x{00C4}S)(TOISTA)?)|KYMMENES|YHDESTOISTA|KAHDESTOISTA) LUKU)$/)
     {
-	$line = "<part> ".$line;
+	if ($first_part_encountered eq "false")
+	{
+	    $line = "###C<part> ".$line;
+	    $first_part_encountered = "true";
+	}
+	else
+	{
+	    $line = "###C<\/paragraph>\n###C<\/section>\n###C<\/chapter>\n###C<\/part>\n###C<part> ".$line;
+	}
 	$latest_element="<part>";
+	$first_chapter_of_part = "true";
     }
-    elsif ($line =~ /^([A-Z\x{00C5}\x{00C4}\x{00D6}]+( [A-Z\x{00C5}\x{00C4}\x{00D6}]+)*)$/)
+    elsif ($first_part_encountered eq "true" && $line =~ /^([A-Z\x{00C5}\x{00C4}\x{00D6}]+( [A-Z\x{00C5}\x{00C4}\x{00D6}]+)*)$/)
     {
-	$line = "<chapter> ".$line;
+	if ($first_chapter_of_part eq "true")
+	{
+	    $line = "###C<chapter> ".$line;
+	    $first_chapter_of_part = "false";
+	}
+	else
+	{
+	    $line = "###C<\/paragraph>\n###C<\/section>\n###C<\/chapter>\n###C<chapter> ".$line;
+	}
 	$latest_element="<chapter>";
+	$first_section_of_chapter = "true";
     }
-    elsif ($line =~ /^ *$/)
+    elsif ($first_part_encountered eq "true" && $line =~ /^ *$/)
     {
 	$empty_lines++;
     }
-    else
+    elsif ($first_part_encountered eq "true")
     {
 	if ($empty_lines > 1)
 	{
-	    $line = "<section>\n<paragraph>\n".$line;
+	    if ($first_section_of_chapter eq "true")
+	    {
+		$line = "###C<section>\n###C<paragraph>\n".$line;
+		$first_section_of_chapter = "false";
+	    }
+	    else
+	    {
+		$line = "###C<\/paragraph>\n###C<\/section>\n###C<section>\n###C<paragraph>\n".$line;
+	    }
 	    $latest_element = "<paragraph>";
 	}
 	elsif ($empty_lines == 1)
 	{
-	    $line = "<paragraph>\n".$line;
+	    $line = "###C<\/paragraph>\n###C<paragraph>\n".$line;
 	    $latest_element = "<paragraph>";
 	}
 	$empty_lines = 0;
@@ -42,3 +71,5 @@ foreach my $line ( <STDIN> ) {
     
     print $line;
 }
+
+print "###C<\/paragraph>\n###C<\/section>\n###C<\/chapter>\n###C<\/part>\n";
